8dcad30ed42590e704e6ea669ab471e8b721090c
ta-02 <talal.gl2004@gmail.com>
2024-11-13 13:51:23 -0500

    added icons to the footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,20 @@
+import { BrandInstagram } from "@mynaui/icons-react";
+import { BrandLinkedin } from "@mynaui/icons-react";
+
 export default function Footer() {
   return (
-    <footer className="mb-10 px-4 text-center">
+    <footer className="px-4 text-center flex flex-col items-center">
       <small className="mb-2 block text-xs">
         &copy; 2024 WLU Double Degree Club. All rights reserved.
       </small>
-      <p className="text-xs"></p>
+      <p className="flex flex-row items-center text-xs">
+        <a href="https://www.instagram.com/DoubleDegreeswlu/">
+          <BrandInstagram />
+        </a>
+        <a href="https://ca.linkedin.com/in/double-degrees-306b59302">
+          <BrandLinkedin />
+        </a>
+      </p>
     </footer>
   );
 }
