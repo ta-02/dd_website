2d05515ef38cd2f5c957a8bab330c1f2a119acd0
ta-02 <talal.gl2004@gmail.com>
2024-11-13 17:15:41 -0500

    fixed small bug

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   );
 }
 
-export function OrbitingCirclesDemo() {
+function OrbitingCirclesDemo() {
   return (
     <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
       <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-black">
